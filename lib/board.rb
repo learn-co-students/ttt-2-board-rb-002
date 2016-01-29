8379e818d343a8900e6f268721bc9b8306d3accc
str8getit <archanllc@outlook.com>
2016-01-29 15:25:11 +0000

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,2 @@
 # Define the variable board below.
-board = ["70 music" "80 music" "90 music" "10 music" "11music" "12music" "13music" "14music" "15music"  ]
+board = [" "," "," "," "," "," "," "," "," "]
