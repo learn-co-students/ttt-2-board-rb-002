1cc182def9ba5d4c9023dbd305d941496cf8b15d
str8getit <archanllc@outlook.com>
2016-02-18 00:15:18 +0000

    nitrous-backup at Thu Feb 18 00:15:18 UTC 2016

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
 # Define the variable board below.
-board = [" "," "," "," "," "," "," "," "," "]
-def display_board
-  puts "A Tic Tac Toe Board"
-end
+board = " "," "," "," "," "," "," "," "," "]
+display_board
+
+
